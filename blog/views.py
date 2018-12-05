3d63bf320aa7f110745b1c47513c236581809b5e
dismalstarshine <i@bzxhn.com>
2018-12-05 22:13:55 +0800

    粗略响应阅读数量

--- blog/views.py
+++ blog/views.py
@@ -3,15 +3,39 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post, Category
 from comments.forms import CommentForm
+from django.views.generic import ListView
 
 
-def index(request):
-    post_list = Post.objects.all()
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+
+class CategoryView(IndexView):
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
+
+
+class ArchivesView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        return super(ArchivesView, self).get_queryset().filter(created_time__year=year, created_time__month=month)
+
+# def index(request):
+#     post_list = Post.objects.all()
+#     return render(request, 'blog/index.html', context={'post_list': post_list})
 
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    post.increase_views()
     post.body = markdown.markdown(post.body, extensions=['markdown.extensions.extra',
                                                          'markdown.extensions.codehilite',
                                                          'markdown.extensions.toc'])
@@ -21,14 +45,14 @@ def detail(request, pk):
     return render(request, 'blog/detail.html', context=context)
 
 
-def archives(request, year, month):
-    post_list = Post.objects.filter(
-        created_time__year=year, created_time__month=month).order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+# def archives(request, year, month):
+#     post_list = Post.objects.filter(
+#         created_time__year=year, created_time__month=month).order_by('-created_time')
+#     return render(request, 'blog/index.html', context={'post_list': post_list})
 
 
-def category(request, pk):
-    cate = get_object_or_404(Category, pk=pk)
-    post_list = Post.objects.filter(category=cate).order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+# def category(request, pk):
+#     cate = get_object_or_404(Category, pk=pk)
+#     post_list = Post.objects.filter(category=cate).order_by('-created_time')
+#     return render(request, 'blog/index.html', context={'post_list': post_list})
 # Create your views here.
